668739efd5bec24b48de5b997357113048a71c9c
Indrajiths4 <indrajithmundackal@gmail.com>
2024-05-13 22:08:55 +0530

    asternity ui

--- my-app/app/(landing)/page.tsx
+++ my-app/app/(landing)/page.tsx
@@ -5,6 +5,8 @@ import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "@/firebaseConfig";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+import { motion } from "framer-motion";
 import {
   Tabs,
   TabsContent,
@@ -44,79 +46,91 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <div className="flex w-full max-w-5xl">
-        <div className="flex-1 mr-8">
-          <img
-            src="./home.jpeg"
-            alt="Your Image"
-            className="w-full h-auto object-cover rounded-lg"
-          />
-        </div>
-        {/* Right Side */}
-        <div className="flex-1 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-          <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-            Welcome to Task Allocator
-          </h2>
-          <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-            Login to task allocator if you can because we don't have a login
-            flow yet
-          </p>
-          <Tabs defaultValue="employee" className="w-[400px] mt-8">
-            <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger value="employee">Employee</TabsTrigger>
-              <TabsTrigger value="company">Company</TabsTrigger>
-            </TabsList>
-            <TabsContent value="employee">
-              <form onSubmit={handleEmployeeSubmit}>
-                {/* Employee sign-in form fields */}
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="email">Email Address</Label>
-                  <Input
-                    id="email"
-                    placeholder="projectmayhem@fc.com"
-                    type="email"
-                  />
-                </LabelInputContainer>
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="password">Password</Label>
-                  <Input id="password" placeholder="••••••••" type="password" />
-                </LabelInputContainer>
-                <button
-                  className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)inset,0px-1px_0px_0px_var(--zinc-800)_inset]"
-                  type="submit"
-                >
-                  Sign in &rarr; <BottomGradient />
-                </button>
-              </form>
-            </TabsContent>
-            <TabsContent value="company">
-              <form onSubmit={handleCompanySubmit}>
-                {/* Company sign-in form fields */}
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="email">Email Address</Label>
-                  <Input
-                    id="email"
-                    placeholder="company@example.com"
-                    type="email"
-                  />
-                </LabelInputContainer>
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="password">Password</Label>
-                  <Input id="password" placeholder="••••••••" type="password" />
-                </LabelInputContainer>
-                <button
-                  className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)inset,0px-1px_0px_0px_var(--zinc-800)_inset]"
-                  type="submit"
-                >
-                  Sign in &rarr; <BottomGradient />
-                </button>
-              </form>
-            </TabsContent>
-          </Tabs>
-        </div>
+    <div className="flex min-h-screen items-center justify-center">
+      <HeroHighlight className="flex-1 mr-8">
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto"
+        >
+          With Task Allocator, efficiency is real. Fair distribution is within reach. It's a perfect match, of {" "}
+          <Highlight className="text-black dark:text-white">
+          skills, of workload, of availability.
+          </Highlight>
+        </motion.h1>
+      </HeroHighlight>
+      <div className="flex-1 max-w-md w-full rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black mx-8">
+        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+          Welcome to Task Allocator
+        </h2>
+        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+          Login to task allocator if you can because we don't have a login flow
+          yet
+        </p>
+        <Tabs defaultValue="employee" className="w-[400px] mt-8">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="employee">Employee</TabsTrigger>
+            <TabsTrigger value="company">Company</TabsTrigger>
+          </TabsList>
+          <TabsContent value="employee">
+            <form onSubmit={handleEmployeeSubmit}>
+              {/* Employee sign-in form fields */}
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="email">Email Address</Label>
+                <Input
+                  id="email"
+                  placeholder="projectmayhem@fc.com"
+                  type="email"
+                />
+              </LabelInputContainer>
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" placeholder="••••••••" type="password" />
+              </LabelInputContainer>
+              <button
+                className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)inset,0px-1px_0px_0px_var(--zinc-800)_inset]"
+                type="submit"
+              >
+                Sign in &rarr; <BottomGradient />
+              </button>
+            </form>
+          </TabsContent>
+          <TabsContent value="company">
+            <form onSubmit={handleCompanySubmit}>
+              {/* Company sign-in form fields */}
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="email">Email Address</Label>
+                <Input
+                  id="email"
+                  placeholder="company@example.com"
+                  type="email"
+                />
+              </LabelInputContainer>
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" placeholder="••••••••" type="password" />
+              </LabelInputContainer>
+              <button
+                className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)inset,0px-1px_0px_0px_var(--zinc-800)_inset]"
+                type="submit"
+              >
+                Sign in &rarr; <BottomGradient />
+              </button>
+            </form>
+          </TabsContent>
+        </Tabs>
       </div>
-    </main>
+    </div>
   );
 }
 
@@ -141,4 +155,4 @@ const LabelInputContainer = ({
       {children}
     </div>
   );
-};
+}
